d54570db06d65c18b2500090ecd388e79ec6166f
Rohan Budhiraja <proyan@users.noreply.github.com>
2021-08-10 15:19:29 +0200

    [ad_fun] extend API exposure

--- include/pycppad/ad_fun.hpp
+++ include/pycppad/ad_fun.hpp
@@ -19,6 +19,7 @@ namespace pycppad
 
     typedef ::CppAD::ADFun<Scalar> ADFun;
     typedef Eigen::Matrix<::CppAD::AD<Scalar>, Eigen::Dynamic, 1> ADVector;
+    typedef Eigen::Matrix<Scalar, Eigen::Dynamic, 1> Vector;
   public:
     
     template<class PyClass>
@@ -27,9 +28,25 @@ namespace pycppad
       cl
 	.def("swap", &ADFun::swap, bp::args("self", "f"))
 	.def("from_json", &ADFun::from_json, bp::args("self", "json"))
+	.def("from_graph", &ADFun::from_graph, bp::args("self", "graph_obj"))
+	.def("to_json", &ADFun::to_json, bp::arg("self"))
 	.def("Dependent", (void (ADFun::*)(const ADVector &, const ADVector &))(&ADFun::Dependent),
-	     bp::args("self", "x", "y"));
-	//.def("to_json", &ADFun::to_json, bp::arg("self"))
+	     bp::args("self", "x", "y"))
+	.def("Forward", (Vector (ADFun::*)(size_t , size_t, const Vector&))(&ADFun::Forward),
+	     bp::args("self", "q", "r", "x"))
+	.def("Forward", (Vector (ADFun::*)(size_t, const Vector&))(&ADFun::Forward),
+	     bp::args("self", "q", "xq"))
+	.def("Reverse", (Vector (ADFun::*)(size_t, const Vector&))(&ADFun::Reverse),
+	     bp::args("self", "p", "v"))
+	.def("Reverse", (Vector (ADFun::*)(size_t, const Vector&))(&ADFun::Reverse),
+	     bp::args("self", "p", "v"))
+	.def("Hessian", (Vector (ADFun::*)(const Vector&,const Vector&))(&ADFun::Hessian),
+	     bp::args("self", "x", "w"))
+	.def("Hessian", (Vector (ADFun::*)(const Vector&, size_t))(&ADFun::Hessian),
+	     bp::args("self", "x", "i"))
+	.def("Jacobian", (Vector (ADFun::*)(const Vector&))(&ADFun::Jacobian),
+	     bp::args("self", "x"))
+	.def("optimize", &ADFun::optimize, bp::args("self", "options"))
 	;
     }
     
