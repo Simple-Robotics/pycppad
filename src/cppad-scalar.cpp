89a52cbd1864eacd33291e556899b6b79c193fc8
Rohan Budhiraja <proyan@users.noreply.github.com>
2021-08-10 14:36:43 +0200

    [cppad][independent] expose more types

--- src/cppad-scalar.cpp
+++ src/cppad-scalar.cpp
@@ -14,16 +14,37 @@ namespace pycppad
   template<typename Scalar>
   void exposeCppADScalar()
   {
-    typedef ::CppAD::AD<Scalar> ADScalar;
+    typedef ::CppAD::AD<Scalar> ADScalar;   
     enum { Options = 0 };
     
-    eigenpy::exposeType<ADScalar>();
-    eigenpy::exposeType<ADScalar,Eigen::RowMajor>();
     pycppad::ADVisitor<Scalar>::expose();
     pycppad::ADFunVisitor<Scalar>::expose();
-    pycppad::IndependentVisitor<Eigen::Matrix<ADScalar,Eigen::Dynamic,1,Options> >::expose();
-    pycppad::IndependentVisitor<Eigen::Matrix<ADScalar,2,1,Options> >::expose();
-    //pycppad::IndependentVisitor<Eigen::Matrix<ADScalar,Eigen::Dynamic,1,Eigen::RowMajor> >::expose();
+    eigenpy::exposeType<ADScalar>();
+    eigenpy::exposeType<ADScalar,Eigen::RowMajor>();
     
+    EIGENPY_MAKE_TYPEDEFS_ALL_SIZES(ADScalar,Options,ads);
+
+    pycppad::IndependentVisitor<Vector2ads>::expose();
+    pycppad::IndependentVisitor<RowVector2ads>::expose();
+    pycppad::IndependentVisitor<Matrix2ads>::expose();
+    pycppad::IndependentVisitor<Matrix2Xads>::expose();
+    pycppad::IndependentVisitor<MatrixX2ads>::expose();
+    
+    pycppad::IndependentVisitor<Vector3ads>::expose();
+    pycppad::IndependentVisitor<RowVector3ads>::expose();
+    pycppad::IndependentVisitor<Matrix3ads>::expose();
+    pycppad::IndependentVisitor<Matrix3Xads>::expose();
+    pycppad::IndependentVisitor<MatrixX3ads>::expose();
+    
+    pycppad::IndependentVisitor<Vector4ads>::expose();
+    pycppad::IndependentVisitor<RowVector4ads>::expose();
+    pycppad::IndependentVisitor<Matrix4ads>::expose();
+    pycppad::IndependentVisitor<Matrix4Xads>::expose();
+    pycppad::IndependentVisitor<MatrixX4ads>::expose();
+    
+    pycppad::IndependentVisitor<VectorXads>::expose();
+    pycppad::IndependentVisitor<RowVectorXads>::expose();
+    pycppad::IndependentVisitor<MatrixXads>::expose();
+
   }
 }
