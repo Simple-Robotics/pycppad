c361395ab17f3180f5ae33266540f44aa5193025
Justin Carpentier <justin.carpentier@inria.fr>
2021-08-11 14:50:16 +0200

    expose: only expose the dynamic types

--- include/pycppad/cppad-scalar.hpp
+++ include/pycppad/cppad-scalar.hpp
@@ -11,42 +11,23 @@
 
 #include <eigenpy/eigenpy.hpp>
 
-
-#define PYCPPAD_MAKE_VECTOR_TYPEDEFS(Type, TypeSuffix, Size, SizeSuffix) \
-  /** \ingroup matrixtypedefs */                                    \
-  typedef Eigen::Matrix<Type, Size, 1>    Vector##SizeSuffix##TypeSuffix; \
-  /** \ingroup matrixtypedefs */                                    \
-  typedef Eigen::Matrix<Type, 1, Size>    RowVector##SizeSuffix##TypeSuffix;
-
-#define PYCPPAD_MAKE_VECTOR_TYPEDEFS_ALL_SIZES(Type, TypeSuffix) \
-  PYCPPAD_MAKE_VECTOR_TYPEDEFS(Type, TypeSuffix, 2, 2)			 \
-  PYCPPAD_MAKE_VECTOR_TYPEDEFS(Type, TypeSuffix, 3, 3) \
-  PYCPPAD_MAKE_VECTOR_TYPEDEFS(Type, TypeSuffix, 4, 4) \
-  PYCPPAD_MAKE_VECTOR_TYPEDEFS(Type, TypeSuffix, Eigen::Dynamic, X)
-
 namespace pycppad
 {
   template<typename Scalar>
   void exposeCppADScalar()
   {
     typedef ::CppAD::AD<Scalar> ADScalar;
+    typedef Eigen::Matrix<ADScalar,Eigen::Dynamic,1> VectorAD;
+    typedef Eigen::Matrix<ADScalar,1,Eigen::Dynamic> RowVectorAD;
+    
     eigenpy::exposeType<ADScalar>();
     eigenpy::exposeType<ADScalar,Eigen::RowMajor>();
 
-    
     pycppad::ADVisitor<Scalar>::expose();
     pycppad::ADFunVisitor<Scalar>::expose();
-    
-    PYCPPAD_MAKE_VECTOR_TYPEDEFS_ALL_SIZES(ADScalar,ads);
 
-    pycppad::IndependentVisitor<Vector2ads>::expose();
-    pycppad::IndependentVisitor<RowVector2ads>::expose();
-    pycppad::IndependentVisitor<Vector3ads>::expose();
-    pycppad::IndependentVisitor<RowVector3ads>::expose();
-    pycppad::IndependentVisitor<Vector4ads>::expose();
-    pycppad::IndependentVisitor<RowVector4ads>::expose();
-    pycppad::IndependentVisitor<VectorXads>::expose();
-    pycppad::IndependentVisitor<RowVectorXads>::expose();
+    pycppad::IndependentVisitor<VectorAD>::expose();
+    pycppad::IndependentVisitor<RowVectorAD>::expose();
   }
 }
 
