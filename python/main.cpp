52bbf60b190ab80ec178cc863f3f6dc72e3ae1a1
Justin Carpentier <justin.carpentier@inria.fr>
2021-08-20 11:38:01 +0200

    python: expose version of the project

--- python/main.cpp
+++ python/main.cpp
@@ -9,9 +9,26 @@
 #include "pycppad/ad.hpp"
 #include "pycppad/independent.hpp"
 
+#include <sstream>
+
+inline std::string printVersion(const std::string & delimiter = ".")
+{
+  std::ostringstream oss;
+  oss
+  << PYCPPAD_MAJOR_VERSION << delimiter
+  << PYCPPAD_MINOR_VERSION << delimiter
+  << PYCPPAD_PATCH_VERSION;
+  return oss.str();
+}
+
+namespace bp = boost::python;
 
 BOOST_PYTHON_MODULE(pycppad)
 {
+  bp::docstring_options module_docstring_options(true,true,false);
+  
+  bp::scope().attr("__version__") = printVersion();
+  bp::scope().attr("__raw_version__") = bp::str(PYCPPAD_VERSION);
   
   eigenpy::enableEigenPy();
   pycppad::enablePyCppAD();
